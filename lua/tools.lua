a7869bebcab93c2f3ee00e29dddbfed35634b12d
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2020-11-01 12:29:05 -0800

    Update tools.lua

--- lua/tools.lua
+++ lua/tools.lua
@@ -23,12 +23,7 @@ if vim.fn.executable("rg") then
         local count = #results
         for i=0, count do results[i]=nil end -- clear the table for the next search
     end
-    local function setLL()
-        vim.fn.setloclist({}, 'r', {title = 'Search Results', lines = results})
-        api.nvim_command('lwindow')
-        local count = #results
-        for i=0, count do results[i]=nil end -- clear the table for the next search
-    end
+    -- local grepprg = vim.api.nvim_buf_get_option(bufnr, "grepprg")
     function M.asyncGrep(term)
         local stdout = vim.loop.new_pipe(false)
         local stderr = vim.loop.new_pipe(false)
@@ -43,26 +38,6 @@ if vim.fn.executable("rg") then
                     stderr:close()
                     handle:close()
                     setQF()
-                end
-            )
-        )
-        vim.loop.read_start(stdout, onread)
-        vim.loop.read_start(stderr, onread)
-    end
-    function M.asyncLgrep(term)
-        local stdout = vim.loop.new_pipe(false)
-        local stderr = vim.loop.new_pipe(false)
-        handle = vim.loop.spawn('rg', {
-                args = {term, '--vimgrep', '--smart-case', '--no-heading'},
-                stdio = {stdout,stderr}
-            },
-            vim.schedule_wrap(function()
-                    stdout:read_stop()
-                    stderr:read_stop()
-                    stdout:close()
-                    stderr:close()
-                    handle:close()
-                    setLL()
                 end
             )
         )
